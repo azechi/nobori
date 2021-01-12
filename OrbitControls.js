723f1678e9f397f8460415ca20d8dd76c6be32ea
azechi <azechi.n@gmail.com>
2021-01-12 10:16:34 +0900

    snowpackがimportのパースに失敗するのでコメントの位置を変更した

--- OrbitControls.js
+++ OrbitControls.js
@@ -12,9 +12,10 @@ import {
 	TOUCH,
 	Vector2,
 	Vector3
-/* 改変箇所 */
-// } from '../../../build/three.module.js';
 } from 'three';
+// 改変箇所
+// } from '../../../build/three.module.js';
+
 
 // This set of controls performs orbiting, dollying (zooming), and panning.
 // Unlike TrackballControls, it maintains the "up" direction object.up (+Y by default).
