7eb58269e3aa737199b3747d4b171ac20355c4c9
azechi <azechi.n@gmail.com>
2021-01-12 01:08:17 +0000

    add snowpack.config.js

---
+++ snowpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  exclude: [
+    '**/node_modules/**/*',
+    '**/web_modules/**/*',
+    '**/.types/**/*',
+    'snowpack.config.js',
+    '**/.git/**/*',
+    '.gitignore',
+    'package.json',
+    'package-lock.json'
+  ]
+}
