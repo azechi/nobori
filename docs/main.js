d91d0c8165ea2ecea63ce768b1bd4d175bc93d30
azechi <azechi@users.noreply.github.com>
2021-01-13 10:12:28 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -2,7 +2,7 @@
 // 参考にしたファイルのライセンス
 // Copyright © 2010-2021 three.js authors
 // https://raw.githubusercontent.com/mrdoob/three.js/75406c631355980994186c84316c606140ac045c/LICENSE
-
+ 
 import * as THREE from "./_snowpack/pkg/three.js";
 
 import Stats from "./stats.module.js";
