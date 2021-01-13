1b843c2b4f17358947ccad544ba6a083dc09dc90
azechi <azechi.n@gmail.com>
2021-01-13 09:55:58 +0900

    update build options

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,12 +1,8 @@
 module.exports = {
-  exclude: [
-    '**/node_modules/**/*',
-    '**/web_modules/**/*',
-    '**/.types/**/*',
-    'snowpack.config.js',
-    '**/.git/**/*',
-    '.gitignore',
-    'package.json',
-    'package-lock.json'
-  ]
-}
+  mount: {
+    public: "/",
+  },
+  buildOptions: {
+    out: "build/dist",
+  },
+};
