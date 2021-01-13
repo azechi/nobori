3d9b1ea3950c80f2f4241cdaad20557147e34f5c
azechi <azechi.n@gmail.com>
2021-01-13 12:26:07 +0000

    prettier formatting

--- public/particle.js
+++ public/particle.js
@@ -1,20 +1,19 @@
 // 参考にした https://raw.githubusercontent.com/mrdoob/three.js/bbc05ad6538e7014c81008d766f1b742b2cdfdbc/examples/webgl_animation_cloth.html
-// 参考にしたファイルのライセンス 
-// Copyright © 2010-2021 three.js authors 
+// 参考にしたファイルのライセンス
+// Copyright © 2010-2021 three.js authors
 // https://raw.githubusercontent.com/mrdoob/three.js/75406c631355980994186c84316c606140ac045c/LICENSE
 
-import * as THREE from 'three';
-export {Particle}
+import * as THREE from "three";
+export { Particle };
 
 const DAMPING = 0.03;
 const DRAG = 1 - DAMPING;
 
-function Particle( x, y, z, mass, clothFunction ) {
-
+function Particle(x, y, z, mass, clothFunction) {
   this.position = new THREE.Vector3();
   this.previous = new THREE.Vector3();
   this.original = new THREE.Vector3();
-  this.a = new THREE.Vector3( 0, 0, 0 ); // acceleration
+  this.a = new THREE.Vector3(0, 0, 0); // acceleration
   this.mass = mass;
   this.invMass = 1 / mass;
   this.tmp = new THREE.Vector3();
@@ -22,34 +21,26 @@ function Particle( x, y, z, mass, clothFunction ) {
 
   // init
 
-  clothFunction( x, y, this.position ); // position
-  clothFunction( x, y, this.previous ); // previous
-  clothFunction( x, y, this.original );
-
+  clothFunction(x, y, this.position); // position
+  clothFunction(x, y, this.previous); // previous
+  clothFunction(x, y, this.original);
 }
 
 // Force -> Acceleration
-Particle.prototype.addForce = function ( force ) {
-
-  this.a.add(
-    this.tmp2.copy( force ).multiplyScalar( this.invMass )
-  );
-
+Particle.prototype.addForce = function (force) {
+  this.a.add(this.tmp2.copy(force).multiplyScalar(this.invMass));
 };
-
 
 // Performs Verlet integration
 
-Particle.prototype.integrate = function ( timesq ) {
-
-  const newPos = this.tmp.subVectors( this.position, this.previous );
-  newPos.multiplyScalar( DRAG ).add( this.position );
-  newPos.add( this.a.multiplyScalar( timesq ) );
+Particle.prototype.integrate = function (timesq) {
+  const newPos = this.tmp.subVectors(this.position, this.previous);
+  newPos.multiplyScalar(DRAG).add(this.position);
+  newPos.add(this.a.multiplyScalar(timesq));
 
   this.tmp = this.previous;
   this.previous = this.position;
   this.position = newPos;
 
-  this.a.set( 0, 0, 0 );
-
+  this.a.set(0, 0, 0);
 };
